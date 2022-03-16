63ed32a12caa8b027dfb4b27c4d8e03541500228
Bhavani G <bhavanimahalakshmi.g@gmail.com>
2022-03-15 18:36:46 -0700

    removed run.sh

--- auto-scaling/ec2_manager.py
+++ auto-scaling/ec2_manager.py
@@ -2,7 +2,7 @@ import boto3
 
 ec2_client = boto3.client('ec2',region_name='us-east-1')
 
-AMI_ID = "ami-0f87d4e6d07287e28"
+AMI_ID = "ami-0e8f09b55f89cd40b"
 ROLE_ARN = "arn:aws:iam::378107157540:instance-profile/sqs-s3-ec2-full-access"
 SECURITY_GROUP = "sg-0dc6c0eb5cc4d9d27"
 KEY_NAME = "Private_Key"
--- run.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-pip3 install -r requirements.txt
-python3 main.py >> /home/ec2-user/app.log
